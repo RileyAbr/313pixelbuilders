1bfec83f773c401a4182970a3c9e097e269b518b
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-04-28 18:48:35 -0500

    Created Nav-Sidebar

--- pixeled-brewing-co/src/app/app.module.ts
+++ pixeled-brewing-co/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavbarComponent
   ],
   imports: [
     BrowserModule,

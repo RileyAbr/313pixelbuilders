6dfa9caf7cc84b78ecde440244914a6b3a637be4
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-05-01 16:10:36 -0500

    Home Page Carousel & Hours Table

--- pixeled-brewing-co/src/app/home/home.component.ts
+++ pixeled-brewing-co/src/app/home/home.component.ts
@@ -9,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   firstImg: string = "../assets/img/pixeled-photos/DB-2.jpg"
-
   imgArray: any[] = [
     {id: 2, src: "../assets/img/pixeled-photos/KQ.jpg"}
+  ]
+
+  hoursArray: any[] = [
+    {date: 'M-F', startTime: '5pm', endTime: '12pm'},
+    {date: 'Sat', startTime: '12am', endTime: '12pm'},
+    {date: 'Sun', startTime: '12am', endTime: '6pm'}
   ]
 
   constructor() { }

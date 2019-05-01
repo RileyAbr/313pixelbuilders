02ffde46899766fb32ad01fdde2d9b0b55f33316
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-05-01 12:08:05 -0500

    Brew Styles

--- pixeled-brewing-co/src/app/home/home.component.ts
+++ pixeled-brewing-co/src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  firstImg: string = "../assets/img/pixeled-photos/DB-2.jpg"
+
+  imgArray: any[] = [
+    {id: 2, src: "../assets/img/pixeled-photos/KQ.jpg"}
+  ]
 
   constructor() { }
 

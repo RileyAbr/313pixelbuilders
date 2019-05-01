c17890dd500207d24b317f01ca7a502fd8f3a5ab
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-04-30 19:54:04 -0500

    Fully Responsive 2

--- pixeled-brewing-co/src/app/brews/brews.component.ts
+++ pixeled-brewing-co/src/app/brews/brews.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Brew} from '../brews/brews.module';
+
+import { Brew } from '../brews/brews.module';
 
 @Component({
   selector: 'app-brews',
@@ -8,42 +9,58 @@ import {Brew} from '../brews/brews.module';
 })
 export class BrewsComponent implements OnInit {
 
-  brews : Brew[] = [
-    {name : "Galactic Implication",
-      type :"New England IPA",
-      tap : true,
-      volume : 4.8},
-      {name : "Jon Luck Pickard",
-      type :"English Strong Bitter",
-      tap : true,
-      volume : 5.0},
-      {name : "I am Brut",
-      type :"Brut IPA",
-      tap : true,
-      volume : 5.6},
-      {name : "UpgrayeDD",
-      type :"Double IPA",
-      tap : true,
-      volume : 6.8},
-      {name : "Jumpman",
-      type :"Brown Ale",
-      tap : true,
-      volume : 4.6},
-      {name : "Tecmo Super Beer",
-      type :"Blonde Ale",
-      tap : true,
-      volume : 5.5},
-      {name : "Coin-Op",
-      type :"Creme Ale",
-      tap : true,
-      volume : 5.7},
-      {name : "HDS Stout",
-      type :"Irish Stout",
-      tap : true,
-      volume : 4.2}
+  brews: Brew[] = [
+    {
+      name: "Galactic Implication",
+      type: "New England IPA",
+      tap: true,
+      volume: 4.8
+    },
+    {
+      name: "Jon Luck Pickard",
+      type: "English Strong Bitter",
+      tap: true,
+      volume: 5.0
+    },
+    {
+      name: "I am Brut",
+      type: "Brut IPA",
+      tap: true,
+      volume: 5.6
+    },
+    {
+      name: "UpgrayeDD",
+      type: "Double IPA",
+      tap: true,
+      volume: 6.8
+    },
+    {
+      name: "Jumpman",
+      type: "Brown Ale",
+      tap: true,
+      volume: 4.6
+    },
+    {
+      name: "Tecmo Super Beer",
+      type: "Blonde Ale",
+      tap: true,
+      volume: 5.5
+    },
+    {
+      name: "Coin-Op",
+      type: "Creme Ale",
+      tap: true,
+      volume: 5.7
+    },
+    {
+      name: "HDS Stout",
+      type: "Irish Stout",
+      tap: true,
+      volume: 4.2
+    }
   ];
   //A temporary Solution to the database issue. We can use this to test our work for now
-  
+
   constructor() { }
 
   ngOnInit() {

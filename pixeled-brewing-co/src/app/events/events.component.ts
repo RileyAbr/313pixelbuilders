08f0d96d30b6d69e87111f0d8da9ed3754afcd5c
RileyAbr <riley.k.abrahamson@gmail.com>
2019-05-07 13:40:04 -0500

    Attached Events JSON

--- pixeled-brewing-co/src/app/events/events.component.ts
+++ pixeled-brewing-co/src/app/events/events.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {Event} from '../events/events.module';
+
+import eventData from '../events.json';
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
-  event : Event[] = [
-    {name : "Killer Queen Mondays",
-      descr : "Play the Arcade game Killer Queen for free every Monday!",
-      date : "Every Monday",
-      privacy : false}
-  ];
-  //Temporary database placeholder for texting ui/calls to pool of events.
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   public showModal = false;
   selectedEvent: Event;
+
+  event : Event[] = eventData;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
 
   loadModal(event: Event): void {
     this.selectedEvent = event;
